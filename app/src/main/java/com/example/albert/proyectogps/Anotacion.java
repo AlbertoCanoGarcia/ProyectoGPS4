b7d9199b44eabf7ac6dd19e1ab2f888e2a2c5cc6
AlbertoCanoGarcia <36516063+AlbertoCanoGarcia@users.noreply.github.com>
2018-02-15 20:27:55 +0100

    a

--- app/src/main/java/com/example/albert/proyectogps/Anotacion.java
+++ app/src/main/java/com/example/albert/proyectogps/Anotacion.java
@@ -1,11 +1,9 @@
 package com.example.albert.proyectogps;
 
-import java.util.Date;
-
 /**
  * Created by Albert on 15/02/2018.
  */
-// clase para guardar las anotaciones aasdfdsdfd
+// clase para guardar las anotaciones
 public class Anotacion {
         private String  Titulo;
         private String Descripción;
