4fb79c1bcc518e0da6df2f8b9efa7bd6ac4b9c4a
pilar <pilariuxy.xelera96@gmail.com>
2018-02-18 18:15:58 +0100

    Pequeña modificacion

--- app/src/main/java/com/example/albert/proyectogps/ListaNotas.java
+++ app/src/main/java/com/example/albert/proyectogps/ListaNotas.java
@@ -92,7 +92,7 @@ public class ListaNotas extends AppCompatActivity {
 
             public void onItemClick(AdapterView<?> a, View v, int position, long id) {
               int tareaselec = position ;
-              Intent ver = new Intent(ListaNotas.this, NuevaTarea.class);
+              Intent ver = new Intent(ListaNotas.this, DatosTarea.class);
               ver.putExtra("Id",position);
               startActivityForResult(ver, 0);
 
