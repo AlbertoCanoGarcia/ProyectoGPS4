d6ad7d68e361e930192cfc227ff05009d2e53632
pilar <pilariuxy.xelera96@gmail.com>
2018-02-18 18:13:36 +0100

    Al pinchar en una nota nos abre un activity nuevo donde nos muestra todos los datos de esta.

---
+++ app/src/main/java/com/example/albert/proyectogps/DatosTarea.java
@@ -0,0 +1,120 @@
+package com.example.albert.proyectogps;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by pilar on 18/02/2018.
+ */
+
+public class DatosTarea extends AppCompatActivity {
+    EditText fec;
+    EditText hor;
+    EditText tit;
+    EditText pri;
+    EditText des;
+    EditText direc;
+    EditText cat;
+    EditText lat;
+    EditText lon;
+
+    SQLiteDatabase db;
+    int x = 0, i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos_nota);
+        fec = (EditText) findViewById(R.id.etfecha);
+        hor = (EditText) findViewById(R.id.ethora);
+        tit = (EditText) findViewById(R.id.ettitulo);
+        pri = (EditText) findViewById(R.id.etprioridad);
+        des = (EditText) findViewById(R.id.etdescripcion);
+        direc = (EditText) findViewById(R.id.etdireccion);
+        cat = (EditText) findViewById(R.id.etcategoria);
+        lat = (EditText) findViewById(R.id.etlatitud);
+        lon = (EditText) findViewById(R.id.etlongitud);
+
+        db=openOrCreateDatabase("gps_ubicaciones",MODE_PRIVATE,null);
+
+        String sqlLeerTodo = "SELECT * FROM ubicaciones;";
+
+        Cursor c = db.rawQuery(sqlLeerTodo, null);
+
+        c.moveToFirst();
+        while (c != null){
+            x++;
+            c.moveToNext();
+        }
+
+        String titulo [] = new String[x];
+        String desc [] = new String[x];
+        String fecha [] = new String[x];
+        String hora [] = new String[x];
+        String prio [] = new String[x];
+        String cate [] = new String[x];
+        String dir [] = new String[x];
+        String lati [] = new String[x];
+        String longi [] = new String[x];
+
+
+        int indiceTitulo = c.getColumnIndex("titulo");
+        int indiceDescripcion = c.getColumnIndex("descripcion");
+        int indiceFecha = c.getColumnIndex("fecha");
+        int indiceHora = c.getColumnIndex("hora");
+        int indicePrio = c.getColumnIndex("prioridad");
+        int indiceCate = c.getColumnIndex("categoria");
+        int indiceDir = c.getColumnIndex("direccion");
+        int indiceLati = c.getColumnIndex("latitud");
+        int indiceLongi = c.getColumnIndex("longitud");
+
+        // Movemos el cursor al primer resultado
+        c.moveToFirst();
+
+        // Recorremos el resto de resultados
+        while (c != null){
+
+            titulo[i] = c.getString(indiceTitulo);
+            desc[i] = c.getString(indiceDescripcion);
+            fecha[i] = c.getString(indiceFecha);
+            dir[i] = c.getString(indiceDir);
+            cate[i] = c.getString(indiceCate);
+            hora[i] = c.getString(indiceHora);
+            prio[i] = c.getString(indicePrio);
+            lati[i] = c.getString(indiceLati);
+            longi[i] = c.getString(indiceLongi);
+
+            i++;
+            c.moveToNext();
+        }
+
+        Bundle bundle = getIntent().getExtras();
+        int pos=Integer.parseInt(bundle.getString("Id"));
+
+        tit.setText(titulo[pos]);
+        des.setText(titulo[pos]);
+        hor.setText(titulo[pos]);
+        fec.setText(titulo[pos]);
+        direc.setText(titulo[pos]);
+        cat.setText(titulo[pos]);
+        lat.setText(titulo[pos]);
+        lon.setText(titulo[pos]);
+
+        Button btn2 = (Button) findViewById(R.id.volver);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent (v.getContext(), ListaNotas.class);
+                startActivityForResult(intent2, 0);
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/com/example/albert/proyectogps/ListaNotas.java
+++ app/src/main/java/com/example/albert/proyectogps/ListaNotas.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -41,58 +42,68 @@ public class ListaNotas extends AppCompatActivity {
         //db.execSQL("CREATE TABLE IF NOT EXISTS ubicaciones(titulo VARCHAR,prioridad VARCHAR,categoria VARCHAR, " +
            //     "descripcion VARCHAR, direccion VARCHAR, fecha DATE, hora VARCHAR);");
 
-        String sqlLeerTodo = "SELECT * FROM gps_ubicaciones;";
+        String sqlLeerTodo = "SELECT * FROM ubicaciones;";
 
-        Cursor c = db.rawQuery(sqlLeerTodo, null);
-        c.moveToFirst();
-        while (c != null){
-            x++;
-            c.moveToNext();
+        try{
+
+            Cursor c = db.rawQuery(sqlLeerTodo, null);
+
+            c.moveToFirst();
+            while (c != null){
+                x++;
+                c.moveToNext();
+            }
+
+            String titulo [] = new String[x];
+            String desc [] = new String[x];
+            String fecha [] = new String[x];
+
+            // Obtenemos los índices de las columnas de "nombre" y "edad". Esto nos
+            // permitirá acceder más tarde a estas columnas
+            int indiceTitulo = c.getColumnIndex("titulo");
+            int indiceDescripcion = c.getColumnIndex("descripcion");
+            int indiceFecha = c.getColumnIndex("fecha");
+            // Movemos el cursor al primer resultado
+            c.moveToFirst();
+
+            Drawable respi = getResources().getDrawable(R.drawable.imgnota, getTheme()); //Instanciamos una imagen ubicada en drawable
+
+            // Recorremos el resto de resultados
+            while (c != null){
+                titulo[i] = c.getString(indiceTitulo);
+                desc[i] = c.getString(indiceDescripcion);
+                fecha[i] = c.getString(indiceFecha);
+
+                cat=new Category(""+ i, titulo[i],desc[i],fecha[i],respi); //colocamos los valores en el constructor de category
+
+                category.add(cat); //Agregamos la variable cat instanciada mas arriba al arrayList
+
+                i++;
+                c.moveToNext();
+            }
+
+            listv=(ListView) findViewById(R.id.listNot); //Inicializamos el ListView
+
+            AdapterItem adapter = new AdapterItem(this, category);
+            listv.setAdapter(adapter);
+            listv.setClickable(true);
+
+            listv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+              int tareaselec = position ;
+              Intent ver = new Intent(ListaNotas.this, NuevaTarea.class);
+              ver.putExtra("Id",position);
+              startActivityForResult(ver, 0);
+
+            }
+            });
+
+
         }
-
-        String titulo [] = new String[x];
-        String desc [] = new String[x];
-        String fecha [] = new String[x];
-
-        // Obtenemos los índices de las columnas de "nombre" y "edad". Esto nos
-        // permitirá acceder más tarde a estas columnas
-        int indiceTitulo = c.getColumnIndex("titulo");
-        int indiceDescripcion = c.getColumnIndex("descripcion");
-        int indiceFecha = c.getColumnIndex("fecha");
-        // Movemos el cursor al primer resultado
-        c.moveToFirst();
-
-        Drawable respi = getResources().getDrawable(R.drawable.imgnota, getTheme()); //Instanciamos una imagen ubicada en drawable
-
-        // Recorremos el resto de resultados
-        while (c != null){
-            titulo[i] = c.getString(indiceTitulo);
-            desc[i] = c.getString(indiceDescripcion);
-            fecha[i] = c.getString(indiceFecha);
-
-            cat=new Category(""+ i, titulo[i],desc[i],fecha[i],respi); //colocamos los valores en el constructor de category
-
-            category.add(cat); //Agregamos la variable cat instanciada mas arriba al arrayList
-
-            i++;
-            c.moveToNext();
+        catch(Exception ex){
+            Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_SHORT).show();
         }
-
-        listv=(ListView) findViewById(R.id.listNot); //Inicializamos el ListView
-
-        AdapterItem adapter = new AdapterItem(this, category);
-        listv.setAdapter(adapter);
-        listv.setClickable(true);
-
-        //listv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            //public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-              //  int tareaselec = position ;
-              //  Intent ver = new Intent(ListaNotas.this, NuevaTarea.class);
-              //  ver.putExtra("Id",position);
-              //  startActivity(ver);
-            //}
-        //});
 
 
     }
